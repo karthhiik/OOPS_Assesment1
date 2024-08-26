c2b053ff4e832e571d600298f8683e4aec643d79
karthhiik <karthikthatikonda18@gmail.com>
2024-08-26 18:58:36 +0530

    Update Animals.java

--- Animals.java
+++ Animals.java
@@ -1,4 +1,19 @@
-public interface Animals{
-// Prepare a Functoin called Eat() with String Paramater
-  
+interface Animals {
+    // Prepare a Functoin called Eat() with String Paramater
+    public String hunt(String a);
+}
+
+class tiger implements Animals {
+
+    public String hunt(String a) {
+        return a;
+    }
+}
+
+public class action {
+    public static void main(String[] args) {
+        tiger ts = new tiger();
+        String n = ts.hunt("deer");
+        System.out.println(n);
+    }
 }
