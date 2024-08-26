2c6dcdd9fcc57c7762480675907ee9ab9ffda944
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-26 16:43:02 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+// 1)Prepare an Object of Emp() and Initialize a Variable
+// 2)Print Values using Getters()
+// 3)Prepare an Object of Emp() Initialize it through Args()Constructor
+// 4)Print Values by using toString();
+  
+}
